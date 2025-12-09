9a1296c079e6192f332bee9db8d621e98e8e636a
cmdrumright <cdrumright@protonmail.com>
2025-12-09 14:26:38 -0600

    added SubmissionButton - made SubmissionButton.js and exported button function - imported function in main.js and add html output to render function - exporting function in transientState to handle survey saving - imported saveSurvey and add to click event - tested and checked database

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -10,3 +10,18 @@ export const setOwnsBlueJeans = (chosenOwnership) => {
 export const setSocioLocationId = (chosenLocation) => {
     transientState.socioLocationId = chosenLocation
 }
+
+export const saveSurveySubmission = async () => {
+    console.log("Saving survey to database...")
+    console.log(transientState)
+    // Create the options for fetch()
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(transientState)
+    }
+    // Send the data to the API
+    const response = await fetch("http://localhost:8088/submissions", postOptions)
+}
